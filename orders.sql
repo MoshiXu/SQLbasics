825f462894db4dc28b0c531e0337f7078316d6c1
Moshi Xu <moshixu92@gmail.com>
2019-08-29 23:35:13 -0500

    Add files via upload

---
+++ orders.sql
@@ -0,0 +1,13 @@
+create table Orders(onum number,amt number,odate date,cnum number,snum number);
+
+insert into orders(onum,amt,odate,cnum,snum) values(3001,18.69,'03-OCT-94',2008,1007);
+insert into orders(onum,amt,odate,cnum,snum) values(3003,767.19,'03/oct/94',2001,1001);
+insert into orders(onum,amt,odate,cnum,snum) values(3002,1900.10,'03/October/1994',2007,1004);
+insert into orders(onum,amt,odate,cnum,snum) values(3005 , 5160.45,'03-OCT-94',2003,1002);
+insert into orders(onum,amt,odate,cnum,snum) values(3006  ,  1098.16,'04-OCT-94',2008 , 1007);
+insert into orders(onum,amt,odate,cnum,snum) values(3009 ,1713.23 ,'04-OCT-94',2002,1003);
+insert into orders(onum,amt,odate,cnum,snum) values(3007,75.75,'05-OCT-94',2004,1002);
+insert into orders(onum,amt,odate,cnum,snum) values(3008,4723.00,'05-OCT-94 ',2006,1001);
+insert into orders(onum,amt,odate,cnum,snum) values(3010,1309.95,'06-OCT-94',2004,1002);
+insert into orders(onum,amt,odate,cnum,snum) values(3011,9891.88,'06-OCT-94',2006,1001);
+
