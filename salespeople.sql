d9cdd3df88dc199dbd504a9cf57f2a39b816d6ca
Moshi Xu <moshixu92@gmail.com>
2019-08-29 23:24:31 -0500

    Add files via upload
    salespeople

---
+++ salespeople.sql
@@ -0,0 +1,15 @@
+create table salespeople(snum number(10),sname varchar(20),city varchar(20),comm number(5,3));
+
+drop table salespeople;
+
+select * from salespeople;
+
+insert into salespeople(snum,sname,city,comm) values(10001,'Peel','London',.12);
+insert into salespeople(snum,sname,city,comm) values(1002,'Serres','San Jose',.13);
+insert into salespeople(snum,sname,city,comm) values(1003,'Motika','London',.11);
+insert into salespeople(snum,sname,city,comm) values(1004,'Rafkin','Barcelona',.15);
+insert into salespeople(snum,sname,city,comm) values(1005,'Axelrod','New York',.1);
+
+
+
+
