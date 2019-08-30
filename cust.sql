c4787a474f7aee13fa09fa0518a7f7e19602573a
Moshi Xu <moshixu92@gmail.com>
2019-08-29 23:27:57 -0500

    Add files via upload

---
+++ cust.sql
@@ -0,0 +1,9 @@
+create table cust (cnum int,cname varchar(10),city varchar(10),rating int,snum int);
+
+insert into cust(cnum,cname,city,rating,snum) VALUES (2001,'Hoffman','London',100,1001);
+insert into cust(cnum,cname,city,rating,snum) VALUES (2002,'Giovanne','Rome',200,1003);
+insert into cust(cnum,cname,city,rating,snum) VALUES (2003,'Liu','San Jose',300,1002);
+insert into cust(cnum,cname,city,rating,snum) VALUES (2004,'Grass','Berlin',100,1002);
+insert into cust(cnum,cname,city,rating,snum) VALUES (2005,'Clemens','London',300,1007);
+insert into cust(cnum,cname,city,rating,snum) VALUES (2006,'Pereira','London',100,1004);
+
