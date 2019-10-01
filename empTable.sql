eccadc74b44fda38298138d54335876455e9ba71
Moshi Xu <moshixu92@gmail.com>
2019-10-01 09:50:45 -0500

    Update empTable.sql

--- empTable.sql
+++ empTable.sql
@@ -2,5 +2,7 @@
 create sequence emp_id start with 1000 increment by 1;
 create table empTable(id number,empname varchar(20));
 insert into empTable(id,empname) values(emp_id.nextval,'peter');
-
+insert into empTable(id,empname) values(emp_id.nextval,'alex');
+insert into empTable(id,empname) values(emp_id.nextval,'John');
+                                                
 select * from emptable;
