746724941e0a4cc0c2e732be67f123c02ace7543
Moshi Xu <moshixu92@gmail.com>
2019-10-03 11:28:13 -0500

    Update orders.sql

--- orders.sql
+++ orders.sql
@@ -1,10 +1,21 @@
 create table Orders(onum number,amt number,odate date,cnum number,snum number);
 
+create table orders(
+        onum number,
+        amt decimal(8,2),
+        odate date,
+        cnum number,
+        snum number,
+        primary key(onum),
+        foreign key(cnum) references cust(cnum) on delete cascade,
+        foreign key(snum) references salespeople(snum) on delete cascade
+);
+
 insert into orders(onum,amt,odate,cnum,snum) values(3001,18.69,'03-OCT-94',2008,1007);
 insert into orders(onum,amt,odate,cnum,snum) values(3003,767.19,'03/oct/94',2001,1001);
 insert into orders(onum,amt,odate,cnum,snum) values(3002,1900.10,'03/October/1994',2007,1004);
 insert into orders(onum,amt,odate,cnum,snum) values(3005 , 5160.45,'03-OCT-94',2003,1002);
-insert into orders(onum,amt,odate,cnum,snum) values(3006  ,  1098.16,'04-OCT-94',2008 , 1007);
+insert into orders(onum,amt,odate,cnum,snum) values(3006Â  ,  1098.16,'04-OCT-94',2008 , 1007);
 insert into orders(onum,amt,odate,cnum,snum) values(3009 ,1713.23 ,'04-OCT-94',2002,1003);
 insert into orders(onum,amt,odate,cnum,snum) values(3007,75.75,'05-OCT-94',2004,1002);
 insert into orders(onum,amt,odate,cnum,snum) values(3008,4723.00,'05-OCT-94 ',2006,1001);
