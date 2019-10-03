997deff2cdf6e4810a5c1ffff32dc5c7bb7ee367
Moshi Xu <moshixu92@gmail.com>
2019-10-03 11:25:20 -0500

    Update cust.sql

--- cust.sql
+++ cust.sql
@@ -1,5 +1,15 @@
 create table cust (cnum int,cname varchar(10),city varchar(10),rating int,snum int);
 
+create table cust (
+        cnum number,
+        cname varchar(20),
+        city varchar(20),
+        rating number,
+        snum number,
+        primary key(cnum),
+        foreign key(snum) references salespeople(snum) on delete cascade
+);
+
 insert into cust(cnum,cname,city,rating,snum) VALUES (2001,'Hoffman','London',100,1001);
 insert into cust(cnum,cname,city,rating,snum) VALUES (2002,'Giovanne','Rome',200,1003);
 insert into cust(cnum,cname,city,rating,snum) VALUES (2003,'Liu','San Jose',300,1002);
