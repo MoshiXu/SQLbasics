b40a3de1531130fb4d90c01b02eb08b1873f6a27
Moshi Xu <moshixu92@gmail.com>
2020-01-16 01:16:39 -0500

    Update ccgg.sql

--- ccgg.sql
+++ ccgg.sql
@@ -2,3 +2,5 @@ INSERT INTO "XDB"."CCGG_USER_PROFILE" (ID, TYPE) VALUES ('1', 'ROLE ADMIN')
 INSERT INTO "XDB"."CCGG_USER_PROFILE" (ID, TYPE) VALUES ('2', 'ROLE USER')
 
 select * from ccgg_user_profile;
+
+select * from ccgg_user;
